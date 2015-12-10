00c53345c3f4dfd4952bb1ce81c3a94f18092708
zbonaker <zbonaker@gmail.com>
2015-12-10 11:12:05 -0800

    New solution with methods for drawing the grid & updating the grid.

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -8,55 +8,39 @@ namespace TicTacToe
 {
     class Program
     {
+        private static string[] gameGrid = new string[] { "_", "1", "_|_", "2", "_|_", "3", "_", //first row of grid (index 0 - 6, count 7)
+                                "_", "4", "_|_", "5", "_|_", "6", "_", //second row of grid (index 7 - 13; count 7
+                                " ", "7", " | ", "8", " | ", "9" }; //third row of grid (index 14 - 19; count 6)
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tic Tac Toe!");
             Console.WriteLine("There are 19,683 board layouts possible. Which one will you play?");
-            Console.WriteLine("");
+            Console.WriteLine();
             Console.WriteLine("What is your name?");
 
             string playerName = Console.ReadLine();
 
-            Console.WriteLine("");
+            Console.WriteLine();
             Console.WriteLine("Hi {0}; let's begin!", playerName);
-            Console.WriteLine("");
-
-            string[] gameGrid = new string[] { "_", "1", "_|_", "2", "_|_", "3", "_", //first row of grid (index 0 - 6, count 7)
-                                "_", "4", "_|_", "5", "_|_", "6", "_", //second row of grid (index 7 - 13; count 7
-                                " ", "7", " | ", "8", " | ", "9" }; //third row of grid (index 14 - 19; count 6)
-
-            for (int i = 0; i < 7; i++)
-            {
-                string grid = gameGrid[i];
-                Console.Write(grid);
-            }
             Console.WriteLine();
-            for (int i = 7; i < 14; i++)
-            {
-                string grid = gameGrid[i];
-                Console.Write(grid);
-            }
-            Console.WriteLine();
-            for (int i = 14; i < 20; i++)
-            {
-                string grid = gameGrid[i];
-                Console.Write(grid);
-            }
 
-            Console.WriteLine("\n");
+            DrawGameGrid();
+
             Console.WriteLine("Select a number to place your X:");
             
             var userInput = Console.ReadLine();
 
-            for (int i = 0; i < 20; i++)
-            {
-                if (userInput == gameGrid[i])
-                {
-                    gameGrid[i] = "X";
-                }
-            }
+            UserMoveUpdateArray(userInput);
 
-            Console.WriteLine("\n");
+            Console.WriteLine();
+            DrawGameGrid();
+
+            Console.ReadLine();
+        }
+
+        public static void DrawGameGrid()
+        {
             for (int i = 0; i < 7; i++)
             {
                 string grid = gameGrid[i];
@@ -75,7 +59,18 @@ namespace TicTacToe
                 Console.Write(grid);
             }
 
-            Console.ReadLine();
+            Console.WriteLine("\n");
+        }
+
+        public static void UserMoveUpdateArray(string evalUserInput)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                if (evalUserInput == gameGrid[i])
+                {
+                    gameGrid[i] = "X";
+                }
+            }
         }
     }
 }
